e776ad3d7d66af831ed72e8eceda3fba2aa7885b
laith977 <lith.adnan@yahoo.com>
2024-02-23 23:56:23 +0300

    Update categories.js

--- constants/categories.js
+++ constants/categories.js
@@ -1,7 +1,7 @@
 const categories = [
   { image: "/images/gasoline.jpg", alt: "gasoline", label: "وقود" },
   { image: "/images/electric.jpg", alt: "electric", label: "كهربائية" },
-  { image: "/images/pick-up.png", alt: "pick-up", label: "البيك أب" },
+  { image: "/images/pick-up.png", alt: "pick-up", label: "بيك اب" },
   { image: "/images/hybrid.jpg", alt: "hybrid", label: "هايبرد" },
   { image: "/images/buses.png", alt: "buses", label: "باصات" },
   { image: "/images/lorrys.jpg", alt: "lorrys", label: "شاحنات نقل" },
